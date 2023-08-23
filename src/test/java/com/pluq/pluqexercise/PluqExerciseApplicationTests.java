c1f3476e769d8056f7aed4bb699511e4c6b6ed15
Anderson Purić de Albuquerque <andersonpuric@gmail.com>
2023-08-23 13:54:46 +0200

    spring boot skeleton project

---
+++ src/test/java/com/pluq/pluqexercise/PluqExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluq.pluqexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluqExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
