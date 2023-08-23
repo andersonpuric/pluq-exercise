00f04de4ffd4bdc372c8750ba149c7261f89892b
Anderson Purić de Albuquerque <andersonpuric@gmail.com>
2023-08-23 17:23:34 +0200

    implement energy price service and controller

--- src/main/java/com/pluq/pluqexercise/controller/EnergyPriceController.java
+++ src/main/java/com/pluq/pluqexercise/controller/EnergyPriceController.java
@@ -1,10 +1,35 @@
 package com.pluq.pluqexercise.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pluq.pluqexercise.model.EnergyPrice;
+import com.pluq.pluqexercise.service.EnergyPriceService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/energy-prices")
+@AllArgsConstructor
 public class EnergyPriceController {
+
+    private final EnergyPriceService energyPriceService;
+
+    @GetMapping("/list")
+    public Iterable<EnergyPrice> list() {
+        return energyPriceService.list();
+    }
+
+    @GetMapping("/{id}")
+    public EnergyPrice getById(@PathVariable Long id) {
+        return energyPriceService.getById(id);
+    }
+
+    @PostMapping
+    public EnergyPrice save(@RequestBody EnergyPrice energyPrice) {
+        return energyPriceService.save(energyPrice);
+    }
+
+    @PostMapping
+    public Iterable<EnergyPrice> saveAll() {
+        return energyPriceService.saveAll();
+    }
 }
--- src/main/java/com/pluq/pluqexercise/service/EnergyPriceService.java
+++ src/main/java/com/pluq/pluqexercise/service/EnergyPriceService.java
@@ -1,7 +1,51 @@
 package com.pluq.pluqexercise.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pluq.pluqexercise.model.EnergyPrice;
+import com.pluq.pluqexercise.repository.EnergyPriceRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class EnergyPriceService {
+
+    private final EnergyPriceRepository energyPriceRepository;
+
+    public Iterable<EnergyPrice> list() {
+        return energyPriceRepository.findAll();
+    }
+
+    public EnergyPrice getById(Long id) {
+        return energyPriceRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public EnergyPrice save(EnergyPrice energyPrice) {
+        return energyPriceRepository.save(energyPrice);
+    }
+
+    public Iterable<EnergyPrice> saveAll() {
+        /*
+            In this part, I mapped the contents of the json file just to save all at once in the database.
+            I made this way to gain some time.
+            In a real application, I wouldn't do this
+         */
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<EnergyPrice>> typeReference = new TypeReference<>() {
+        };
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/energyPrices.json")) {
+            List<EnergyPrice> energyPrices = objectMapper.readValue(inputStream, typeReference);
+            return energyPriceRepository.saveAll(energyPrices);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
