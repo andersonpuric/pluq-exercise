055f96dbd40cca9fea1847178d75c74cb873411c
Anderson Purić de Albuquerque <andersonpuric@gmail.com>
2023-08-23 23:18:31 +0200

    remove annotation from test

--- src/test/java/com/pluq/pluqexercise/utils/TestJsonFileParser.java
+++ src/test/java/com/pluq/pluqexercise/utils/TestJsonFileParser.java
@@ -2,12 +2,9 @@ package com.pluq.pluqexercise.utils;
 
 import com.pluq.pluqexercise.model.Location;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-@ExtendWith(SpringExtension.class)
 public class TestJsonFileParser {
 
     @Test
